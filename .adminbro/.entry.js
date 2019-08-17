6f260f49401df044cbab67dad74de22ca0c6faf7
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-08-17 10:56:34 +0200

    update docs

---
+++ .adminbro/.entry.js
@@ -0,0 +1,9 @@
+AdminBro.UserComponents = {}
+import Component1 from '/usr/src/app/admin-bro-example-app/admin/components/detailed-stats'
+AdminBro.UserComponents.Component1 = Component1
+import Component2 from '/usr/src/app/admin-bro-example-app/admin/components/dont-touch-this-action'
+AdminBro.UserComponents.Component2 = Component2
+import Component3 from '/usr/src/app/admin-bro-example-app/admin/components/article-in-list'
+AdminBro.UserComponents.Component3 = Component3
+import Component4 from '/usr/src/app/admin-bro-example-app/admin/components/dashboard'
+AdminBro.UserComponents.Component4 = Component4
