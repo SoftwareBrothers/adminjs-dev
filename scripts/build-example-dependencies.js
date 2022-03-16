d0185682e5b3c9866a012a7e3fd5c0f449eff090
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-16 08:51:36 +0100

    chore: moved example app outside packages

--- scripts/build-example-dependencies.js
+++ scripts/build-example-dependencies.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { exec } = require('child_process');
 
-const runWorkspaceScript = (workspace, script) => {
+function runWorkspaceScript(workspace, script) {
   exec(`yarn workspace ${workspace} ${script}`, (err, stdout, stderr) => {
     if (err) {
       console.log(stderr);
@@ -9,16 +9,11 @@ const runWorkspaceScript = (workspace, script) => {
     }
     console.log(`Running ${workspace} ${script} finished`);
   });
-};
+}
 
-const examplePackagePath = '../packages/adminjs-example-app/package.json';
-const package = require(path.join(__dirname, examplePackagePath));
+const examplePackagePath = '../adminjs-example-app/package.json';
+const { dependencies } = require(path.join(__dirname, examplePackagePath));
 
-const dependencies = Object.keys(package.dependencies);
-const adminDependencies = dependencies.filter((d) => {
-  return /@adminjs/.test(d);
-});
-
-adminDependencies.forEach((workspace) => {
-  runWorkspaceScript(workspace, 'build');
-});
+Object.keys(dependencies).forEach((workspace) =>
+  /@adminjs/.test(workspace) ? runWorkspaceScript(workspace, 'build') : null
+);
