b0f0944b1446047501735dee03023ee284bdfb75
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-04-27 10:01:15 +0200

    docs: updated docs with koa

--- docs/entry.js
+++ docs/entry.js
@@ -57,173 +57,170 @@ reactComponents['ButtonGroup'] = Component8;
 import Component9 from '../packages/adminjs-design-system/src/atoms/check-box.tsx';
 reactComponents['CheckBox'] = Component9;
 
-import Component10 from '../packages/adminjs-design-system/src/atoms/currency-input.tsx';
-reactComponents['CurrencyInput'] = Component10;
+import Component10 from '../packages/adminjs-design-system/src/organisms/current-user-nav/current-user-nav.tsx';
+reactComponents['CurrentUserNav'] = Component10;
 
-import Component11 from '../packages/adminjs-design-system/src/organisms/current-user-nav/current-user-nav.tsx';
-reactComponents['CurrentUserNav'] = Component11;
+import Component11 from '../packages/adminjs-design-system/src/molecules/date-picker/date-picker.tsx';
+reactComponents['DatePicker'] = Component11;
 
-import Component12 from '../packages/adminjs-design-system/src/molecules/date-picker/date-picker.tsx';
-reactComponents['DatePicker'] = Component12;
+import Component12 from '../packages/adminjs-design-system/src/molecules/drawer/drawer.tsx';
+reactComponents['Drawer'] = Component12;
 
-import Component13 from '../packages/adminjs-design-system/src/molecules/drawer/drawer.tsx';
-reactComponents['Drawer'] = Component13;
+import Component13 from '../packages/adminjs-design-system/src/molecules/drawer/drawer-content.tsx';
+reactComponents['DrawerContent'] = Component13;
 
-import Component14 from '../packages/adminjs-design-system/src/molecules/drawer/drawer-content.tsx';
-reactComponents['DrawerContent'] = Component14;
+import Component14 from '../packages/adminjs-design-system/src/molecules/drawer/drawer-footer.tsx';
+reactComponents['DrawerFooter'] = Component14;
 
-import Component15 from '../packages/adminjs-design-system/src/molecules/drawer/drawer-footer.tsx';
-reactComponents['DrawerFooter'] = Component15;
+import Component15 from '../packages/adminjs/src/frontend/components/app/drawer-portal.tsx';
+reactComponents['DrawerPortal'] = Component15;
 
-import Component16 from '../packages/adminjs/src/frontend/components/app/drawer-portal.tsx';
-reactComponents['DrawerPortal'] = Component16;
+import Component16 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down.tsx';
+reactComponents['DropDown'] = Component16;
 
-import Component17 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down.tsx';
-reactComponents['DropDown'] = Component17;
+import Component17 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down-item.tsx';
+reactComponents['DropDownItem'] = Component17;
 
-import Component18 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down-item.tsx';
-reactComponents['DropDownItem'] = Component18;
+import Component18 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down-menu.tsx';
+reactComponents['DropDownMenu'] = Component18;
 
-import Component19 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down-menu.tsx';
-reactComponents['DropDownMenu'] = Component19;
+import Component19 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down-trigger.tsx';
+reactComponents['DropDownTrigger'] = Component19;
 
-import Component20 from '../packages/adminjs-design-system/src/molecules/drop-down/drop-down-trigger.tsx';
-reactComponents['DropDownTrigger'] = Component20;
+import Component20 from '../packages/adminjs-design-system/src/molecules/drop-zone/drop-zone.tsx';
+reactComponents['DropZone'] = Component20;
 
-import Component21 from '../packages/adminjs-design-system/src/molecules/drop-zone/drop-zone.tsx';
-reactComponents['DropZone'] = Component21;
+import Component21 from '../packages/adminjs-design-system/src/molecules/drop-zone/drop-zone-item.tsx';
+reactComponents['DropZoneItem'] = Component21;
 
-import Component22 from '../packages/adminjs-design-system/src/molecules/drop-zone/drop-zone-item.tsx';
-reactComponents['DropZoneItem'] = Component22;
+import Component22 from '../packages/adminjs/src/frontend/components/app/error-message.tsx';
+reactComponents['ErrorMessageBox'] = Component22;
 
-import Component23 from '../packages/adminjs/src/frontend/components/app/error-message.tsx';
-reactComponents['ErrorMessageBox'] = Component23;
+import Component23 from '../packages/adminjs-design-system/src/molecules/form-group/form-group.tsx';
+reactComponents['FormGroup'] = Component23;
 
-import Component24 from '../packages/adminjs-design-system/src/molecules/form-group/form-group.tsx';
-reactComponents['FormGroup'] = Component24;
+import Component24 from '../packages/adminjs-design-system/src/molecules/form-group/form-message.tsx';
+reactComponents['FormMessage'] = Component24;
 
-import Component25 from '../packages/adminjs-design-system/src/molecules/form-group/form-message.tsx';
-reactComponents['FormMessage'] = Component25;
+import Component25 from '../packages/adminjs-design-system/src/atoms/typography/header.tsx';
+reactComponents['Header'] = Component25;
 
-import Component26 from '../packages/adminjs-design-system/src/atoms/typography/header.tsx';
-reactComponents['Header'] = Component26;
+import Component26 from '../packages/adminjs-design-system/src/atoms/icon.tsx';
+reactComponents['Icon'] = Component26;
 
-import Component27 from '../packages/adminjs-design-system/src/atoms/icon.tsx';
-reactComponents['Icon'] = Component27;
+import Component27 from '../packages/adminjs-design-system/src/atoms/illustration.tsx';
+reactComponents['Illustration'] = Component27;
 
-import Component28 from '../packages/adminjs-design-system/src/atoms/illustration.tsx';
-reactComponents['Illustration'] = Component28;
+import Component28 from '../packages/adminjs-design-system/src/molecules/info-box.tsx';
+reactComponents['InfoBox'] = Component28;
 
-import Component29 from '../packages/adminjs-design-system/src/molecules/info-box.tsx';
-reactComponents['InfoBox'] = Component29;
+import Component29 from '../packages/adminjs-design-system/src/atoms/input.tsx';
+reactComponents['Input'] = Component29;
 
-import Component30 from '../packages/adminjs-design-system/src/atoms/input.tsx';
-reactComponents['Input'] = Component30;
+import Component30 from '../packages/adminjs-design-system/src/molecules/form-group/input-group.tsx';
+reactComponents['InputGroup'] = Component30;
 
-import Component31 from '../packages/adminjs-design-system/src/molecules/form-group/input-group.tsx';
-reactComponents['InputGroup'] = Component31;
+import Component31 from '../packages/adminjs-design-system/src/atoms/label.tsx';
+reactComponents['Label'] = Component31;
 
-import Component32 from '../packages/adminjs-design-system/src/atoms/label.tsx';
-reactComponents['Label'] = Component32;
+import Component32 from '../packages/adminjs-design-system/src/atoms/link.tsx';
+reactComponents['Link'] = Component32;
 
-import Component33 from '../packages/adminjs-design-system/src/atoms/link.tsx';
-reactComponents['Link'] = Component33;
+import Component33 from '../packages/adminjs-design-system/src/atoms/loader.tsx';
+reactComponents['Loader'] = Component33;
 
-import Component34 from '../packages/adminjs-design-system/src/atoms/loader.tsx';
-reactComponents['Loader'] = Component34;
+import Component34 from '../packages/adminjs-design-system/src/molecules/logged-user.tsx';
+reactComponents['LoggedUser'] = Component34;
 
-import Component35 from '../packages/adminjs-design-system/src/molecules/logged-user.tsx';
-reactComponents['LoggedUser'] = Component35;
+import Component35 from '../packages/adminjs-design-system/src/molecules/message-box.tsx';
+reactComponents['MessageBox'] = Component35;
 
-import Component36 from '../packages/adminjs-design-system/src/molecules/message-box.tsx';
-reactComponents['MessageBox'] = Component36;
+import Component36 from '../packages/adminjs-design-system/src/molecules/modal/modal.tsx';
+reactComponents['Modal'] = Component36;
 
-import Component37 from '../packages/adminjs-design-system/src/molecules/modal/modal.tsx';
-reactComponents['Modal'] = Component37;
+import Component37 from '../packages/adminjs-design-system/src/molecules/modal/modal-inline.tsx';
+reactComponents['ModalInline'] = Component37;
 
-import Component38 from '../packages/adminjs-design-system/src/molecules/modal/modal-inline.tsx';
-reactComponents['ModalInline'] = Component38;
+import Component38 from '../packages/adminjs-design-system/src/molecules/nav-group.tsx';
+reactComponents['NavGroup'] = Component38;
 
-import Component39 from '../packages/adminjs-design-system/src/molecules/nav-group.tsx';
-reactComponents['NavGroup'] = Component39;
+import Component39 from '../packages/adminjs-design-system/src/organisms/navigation/navigation.tsx';
+reactComponents['Navigation'] = Component39;
 
-import Component40 from '../packages/adminjs-design-system/src/organisms/navigation/navigation.tsx';
-reactComponents['Navigation'] = Component40;
+import Component40 from '../packages/adminjs-design-system/src/molecules/navigation-element/navigation-element.tsx';
+reactComponents['NavigationElement'] = Component40;
 
-import Component41 from '../packages/adminjs-design-system/src/molecules/navigation-element/navigation-element.tsx';
-reactComponents['NavigationElement'] = Component41;
+import Component41 from '../packages/adminjs-design-system/src/molecules/pagination.tsx';
+reactComponents['Pagination'] = Component41;
 
-import Component42 from '../packages/adminjs-design-system/src/molecules/pagination.tsx';
-reactComponents['Pagination'] = Component42;
+import Component42 from '../packages/adminjs-design-system/src/atoms/placeholder.tsx';
+reactComponents['Placeholder'] = Component42;
 
-import Component43 from '../packages/adminjs-design-system/src/atoms/placeholder.tsx';
-reactComponents['Placeholder'] = Component43;
+import Component43 from '../packages/adminjs-design-system/src/atoms/radio.tsx';
+reactComponents['Radio'] = Component43;
 
-import Component44 from '../packages/adminjs-design-system/src/atoms/radio.tsx';
-reactComponents['Radio'] = Component44;
+import Component44 from '../packages/adminjs/src/frontend/components/app/records-table/records-table.tsx';
+reactComponents['RecordsTable'] = Component44;
 
-import Component45 from '../packages/adminjs/src/frontend/components/app/records-table/records-table.tsx';
-reactComponents['RecordsTable'] = Component45;
+import Component45 from '../packages/adminjs/src/frontend/components/app/records-table/records-table-header.tsx';
+reactComponents['RecordsTableHeader'] = Component45;
 
-import Component46 from '../packages/adminjs/src/frontend/components/app/records-table/records-table-header.tsx';
-reactComponents['RecordsTableHeader'] = Component46;
+import Component46 from '../packages/adminjs-design-system/src/molecules/rich-text/rich-text.tsx';
+reactComponents['RichText'] = Component46;
 
-import Component47 from '../packages/adminjs-design-system/src/molecules/rich-text/rich-text.tsx';
-reactComponents['RichText'] = Component47;
+import Component47 from '../packages/adminjs-design-system/src/atoms/section.tsx';
+reactComponents['Section'] = Component47;
 
-import Component48 from '../packages/adminjs-design-system/src/atoms/section.tsx';
-reactComponents['Section'] = Component48;
+import Component48 from '../packages/adminjs/src/frontend/components/actions/bulk-delete.tsx';
+reactComponents['ShowAction'] = Component48;
 
-import Component49 from '../packages/adminjs/src/frontend/components/actions/bulk-delete.tsx';
+import Component49 from '../packages/adminjs/src/frontend/components/actions/show.tsx';
 reactComponents['ShowAction'] = Component49;
 
-import Component50 from '../packages/adminjs/src/frontend/components/actions/show.tsx';
-reactComponents['ShowAction'] = Component50;
+import Component50 from '../packages/adminjs/src/frontend/components/app/sidebar/sidebar-resource-section.tsx';
+reactComponents['SidebarResourceSection'] = Component50;
 
-import Component51 from '../packages/adminjs/src/frontend/components/app/sidebar/sidebar-resource-section.tsx';
-reactComponents['SidebarResourceSection'] = Component51;
+import Component51 from '../packages/adminjs-design-system/src/molecules/stepper/step.tsx';
+reactComponents['Step'] = Component51;
 
-import Component52 from '../packages/adminjs-design-system/src/molecules/stepper/step.tsx';
-reactComponents['Step'] = Component52;
+import Component52 from '../packages/adminjs-design-system/src/molecules/stepper/stepper.tsx';
+reactComponents['Stepper'] = Component52;
 
-import Component53 from '../packages/adminjs-design-system/src/molecules/stepper/stepper.tsx';
-reactComponents['Stepper'] = Component53;
+import Component53 from '../packages/adminjs-design-system/src/atoms/table/table.tsx';
+reactComponents['Table'] = Component53;
 
-import Component54 from '../packages/adminjs-design-system/src/atoms/table/table.tsx';
-reactComponents['Table'] = Component54;
+import Component54 from '../packages/adminjs-design-system/src/atoms/table/table-body.tsx';
+reactComponents['TableBody'] = Component54;
 
-import Component55 from '../packages/adminjs-design-system/src/atoms/table/table-body.tsx';
-reactComponents['TableBody'] = Component55;
+import Component55 from '../packages/adminjs-design-system/src/atoms/table/table-caption.tsx';
+reactComponents['TableCaption'] = Component55;
 
-import Component56 from '../packages/adminjs-design-system/src/atoms/table/table-caption.tsx';
-reactComponents['TableCaption'] = Component56;
+import Component56 from '../packages/adminjs-design-system/src/atoms/table/table-cell.tsx';
+reactComponents['TableCell'] = Component56;
 
-import Component57 from '../packages/adminjs-design-system/src/atoms/table/table-cell.tsx';
-reactComponents['TableCell'] = Component57;
+import Component57 from '../packages/adminjs-design-system/src/atoms/table/table-head.tsx';
+reactComponents['TableHead'] = Component57;
 
-import Component58 from '../packages/adminjs-design-system/src/atoms/table/table-head.tsx';
-reactComponents['TableHead'] = Component58;
+import Component58 from '../packages/adminjs-design-system/src/atoms/table/table-row.tsx';
+reactComponents['TableRow'] = Component58;
 
-import Component59 from '../packages/adminjs-design-system/src/atoms/table/table-row.tsx';
-reactComponents['TableRow'] = Component59;
+import Component59 from '../packages/adminjs-design-system/src/atoms/text.tsx';
+reactComponents['Text'] = Component59;
 
-import Component60 from '../packages/adminjs-design-system/src/atoms/text.tsx';
-reactComponents['Text'] = Component60;
+import Component60 from '../packages/adminjs-design-system/src/atoms/text-area.tsx';
+reactComponents['TextArea'] = Component60;
 
-import Component61 from '../packages/adminjs-design-system/src/atoms/text-area.tsx';
-reactComponents['TextArea'] = Component61;
+import Component61 from '../packages/adminjs-design-system/src/atoms/tooltip/tooltip.tsx';
+reactComponents['Tooltip'] = Component61;
 
-import Component62 from '../packages/adminjs-design-system/src/atoms/tooltip/tooltip.tsx';
-reactComponents['Tooltip'] = Component62;
+import Component62 from '../packages/adminjs-design-system/src/atoms/tooltip/tooltip-control.tsx';
+reactComponents['TooltipControl'] = Component62;
 
-import Component63 from '../packages/adminjs-design-system/src/atoms/tooltip/tooltip-control.tsx';
-reactComponents['TooltipControl'] = Component63;
+import Component63 from '../packages/adminjs-design-system/src/atoms/tooltip/tooltip-content.tsx';
+reactComponents['TooltipContent'] = Component63;
 
-import Component64 from '../packages/adminjs-design-system/src/atoms/tooltip/tooltip-content.tsx';
-reactComponents['TooltipContent'] = Component64;
+import Component64 from '../packages/adminjs-design-system/src/molecules/value-group/value-group.tsx';
+reactComponents['ValueGroup'] = Component64;
 
-import Component65 from '../packages/adminjs-design-system/src/molecules/value-group/value-group.tsx';
-reactComponents['ValueGroup'] = Component65;
-
-import Component66 from '../packages/adminjs/src/frontend/hoc/with-notice.ts';
-reactComponents['withNotice'] = Component66;
+import Component65 from '../packages/adminjs/src/frontend/hoc/with-notice.ts';
+reactComponents['withNotice'] = Component65;
